edaa7ffd5f7a7ef5e4eb909543c79f9e7d7894ea
Fagner Sá <fagnermacedo.sa@gmail.com>
2022-04-27 17:37:18 -0300

    Listando os pedidos
    Listando os pedidos na tela.
    Precisa ajustar via CSS a tabela. Os pedidos não estão  posicionados de forma correta. Informações saíram ao lado  do cabeçalho da tabela.

--- service/pedidos-service.js
+++ service/pedidos-service.js
@@ -1,10 +1,31 @@
+const criaNovaLinha = (id, nome, preco, multiplo) => {
+
+    const novaLinhaPedio = document.createElement('tr')
+    const conteudo = ` 
+        <td class="td" data-td>${id}</td>
+            <td>${nome}</td>
+            <td>${preco}</td>
+            <td>${multiplo}</td>
+            <td>
+                      <ul class="tabela__botoes-controle">
+                          <li><a href="../telas/edita_pedidos.html?id=${id}" class="botao-editar">Editar</a></li>
+                          <li><button class="botao-excluir" type="button">Excluir</button></li>
+                      </ul>
+            </td> 
+    `
+    novaLinhaPedio.innerHTML = conteudo
+    return novaLinhaPedio
+}
+
+const tabela = document.querySelector('[data-tabela]')
+
 const http = new XMLHttpRequest()
 
 http.open('GET','http://localhost:3000/profile')
-
 http.send()
-
 http.onload = () => {
-    const data = http.response
-    console.log(data)
+    const data = JSON.parse(http.response)
+    data.forEach(element => {
+        tabela.appendChild(criaNovaLinha(element.id, element.nome, element.preco, element.multiplo))
+    })
 }
