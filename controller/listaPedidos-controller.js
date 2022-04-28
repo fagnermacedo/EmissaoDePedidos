c0470ceae504192083e10490d1ff8ae10ba8c5b5
fagnermacedo <fagnermacedo@yahoo.com.br>
2022-04-27 23:32:24 -0300

    Método de Criar pedido
    Desenvolvido o método de criar pedido.
    Dados saldo no arquivo db.json
    
    Criado mais duas página (ainda não concluída). Edição de Pedido e e Pedido concluído.

--- controller/listaPedidos-controller.js
+++ controller/listaPedidos-controller.js
@@ -1,4 +1,28 @@
-import { pedidosService } from "../service/pedidos-service.js";
+import { pedidosService } from "../service/pedidos-service.js"
+
+const criaNovaLinha = (id, nome, preco, multiplo) => {
+
+    const novaLinhaPedio = document.createElement('tr')
+    const conteudo = ` 
+        <td class="td" data-td>${id}</td>
+            <td>${nome}</td>
+            <td>${preco}</td>
+            <td>${multiplo}</td>
+            <td>
+                      <ul class="tabela__botoes-controle">
+                          <li><a href="../telas/edita_pedidos.html?id=${id}" class="botao-editar">Editar</a></li>
+                          <li><button class="botao-excluir" type="button">Excluir</button></li>
+                      </ul>
+            </td> 
+    `
+    novaLinhaPedio.innerHTML = conteudo
+    return novaLinhaPedio
+}
+
+const tabela = document.querySelector('[data-tabela]')
 
 pedidosService.listaPedidos()
-    .then(console.log("teste"))
+.then(data => {
+    data.forEach(element => {
+    tabela.appendChild(criaNovaLinha(element.id, element.nome, element.preco, element.multiplo))
+})})
--- service/pedidos-service.js
+++ service/pedidos-service.js
@@ -1,46 +1,29 @@
-const criaNovaLinha = (id, nome, preco, multiplo) => {
-
-    const novaLinhaPedio = document.createElement('tr')
-    const conteudo = ` 
-        <td class="td" data-td>${id}</td>
-            <td>${nome}</td>
-            <td>${preco}</td>
-            <td>${multiplo}</td>
-            <td>
-                      <ul class="tabela__botoes-controle">
-                          <li><a href="../telas/edita_pedidos.html?id=${id}" class="botao-editar">Editar</a></li>
-                          <li><button class="botao-excluir" type="button">Excluir</button></li>
-                      </ul>
-            </td> 
-    `
-    novaLinhaPedio.innerHTML = conteudo
-    return novaLinhaPedio
-}
-
-const tabela = document.querySelector('[data-tabela]')
-
 const listaPedidos = () => {
-    const promise = new Promise ((resolve, reject) =>{
-        const http = new XMLHttpRequest()
-        http.open('GET','http://localhost:3000/profile')
-        
-        http.onload = () => {
-            if(http.status >= 400){
-                reject(JSON.parse(http.response))
-            } else {
-                resolve(JSON.parse(http.response))
-            }
-        }   
-        
-        http.send()
+    return fetch(`http://localhost:3000/profile`)
+    .then( resposta => {
+        return resposta.json()
     })
-    console.log("Teste promise")
-    return promise
 }
 
+const criaPedido = (id, nome, preco, multiplo) =>{
+    return fetch(`http://localhost:3000/profile`,{
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+            id: id,
+            nome: nome,
+            preco: preco,
+            multiplo: multiplo
+        })
+    })
+    .then(reposta => {
+        return reposta.body
+    })
+}
 
-listaPedidos()
-.then(data => {
-    data.forEach(element => {
-    tabela.appendChild(criaNovaLinha(element.id, element.nome, element.preco, element.multiplo))
-})})
+export const pedidosService = {
+    listaPedidos,
+    criaPedido
+}
