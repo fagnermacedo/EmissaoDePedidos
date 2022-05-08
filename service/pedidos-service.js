3f088c7bb5d24b8915f5d6810fc993258585142d
Fagner Sá <fagnermacedo.sa@gmail.com>
2022-05-08 11:07:57 -0300

    Alteração endereços URL dos arquivo JSON

--- service/pedidos-service.js
+++ service/pedidos-service.js
@@ -1,12 +1,12 @@
 const listaPedidos = () => {
-    return fetch(`https://raw.githubusercontent.com/fagnermacedo/pedidoscompras/main/db.json`)
+    return fetch(`https://github.com/fagnermacedo/EmissaoDePedidos/blob/main/db.json`)
     .then( resposta => {
         return resposta.json()
     })
 }
 
 const criaPedido = (id, nome, preco, multiplo) =>{
-    return fetch(`https://raw.githubusercontent.com/fagnermacedo/pedidoscompras/main/db.json`,{
+    return fetch(`https://github.com/fagnermacedo/EmissaoDePedidos/blob/main/db.json`,{
         method: 'POST',
         headers: {
             'Content-Type' : 'application/json'
@@ -31,21 +31,21 @@ const removePedido = (id) => {
 }
 
 const carregaPedidoParaEdicao = (id) => {
-    return fetch(`https://raw.githubusercontent.com/fagnermacedo/pedidoscompras/main/db.json/${id}`)
+    return fetch(`https://github.com/fagnermacedo/EmissaoDePedidos/blob/main/db.json/${id}`)
     .then(resposta => {
         return resposta.json()
     })
 }
 
 const carregaClientesParaEdicao = () => {
-    return fetch(`https://raw.githubusercontent.com/fagnermacedo/pedidoscompras/main/db.json`)
+    return fetch(`https://github.com/fagnermacedo/EmissaoDePedidos/blob/main/db.json`)
     .then(resposta => {
         return resposta.json()
     })
 } 
 
 const atualizaPedido = (id, nome, preco, multiplo) => {
-    return fetch(`https://raw.githubusercontent.com/fagnermacedo/pedidoscompras/main/db.json/${id}`, {
+    return fetch(`https://github.com/fagnermacedo/EmissaoDePedidos/blob/main/db.json/${id}`, {
         method: 'PUT',
         headers: {
             'Content-type' : 'application/json'
