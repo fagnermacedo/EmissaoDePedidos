b1eeee9cab0e01dbbfbd31ee16039afb742a6db7
Fagner Sá <fagnermacedo.sa@gmail.com>
2022-05-08 18:25:45 -0300

    Update pedidos-service.js

--- service/pedidos-service.js
+++ service/pedidos-service.js
@@ -1,5 +1,5 @@
 const listaPedidos = () => {
-    return fetch(`https://raw.githubusercontent.com/fagnermacedo/EmissaoDePedidos/main/db.json`,{
+    return fetch(`https://raw.githubusercontent.com/fagnermacedo/pedidoscompras/main/db.json`,{
         mode: 'no-cors'
     })
     .then( resposta => {
